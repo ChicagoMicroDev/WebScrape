9d366b98ad6d1a7cb1989a469e566c55d3b2d840
recklesskidjo3 <josephs409@gmail.com>
2018-10-01 20:16:20 -0500

    Connecting to The database

--- server.js
+++ server.js
@@ -19,6 +19,58 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with request
+    axios.get("https://www.chicagobusiness.com/").then(function(response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+
+
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("a h1").each(function(i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).text();
+                // .children().first()
+                // .data;
+            result.link = $(this).parent("a").attr("href");
+                // .children("a")
+                // .attr("href");
+            console.log(result);
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+        });
+
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scrape Complete");
+    });
+});
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
+
